a9b98396f46d42197ed49bab2937e1e823840000
Jeevan Padwal <105958058+jeevanpadwal@users.noreply.github.com>
2023-01-27 21:12:47 +0530

    Delete Matplot.py

--- Matplot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-excel_file = 'WinePredictor.xlsx'
-
-data = pd.read_excel(excel_file)
-
-data[['Ash','Alcohol']].plot(kind="hist")
-plt.show()
